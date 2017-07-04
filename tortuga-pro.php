962621baa7b6d9d3b9625af7f413df9c10239eba
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-07-04 15:33:00 +0200

    updated translation files and changelog

--- tortuga-pro.php
+++ tortuga-pro.php
@@ -5,7 +5,7 @@ Plugin URI: http://themezee.com/addons/tortuga-pro/
 Description: Adds additional features like footer widgets, custom colors, fonts and logo upload to the Tortuga theme.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.3
+Version: 1.3.1
 Text Domain: tortuga-pro
 Domain Path: /languages/
 License: GPL v3
@@ -62,7 +62,7 @@ class Tortuga_Pro {
 		define( 'TORTUGA_PRO_NAME', 'Tortuga Pro' );
 
 		// Define Version Number.
-		define( 'TORTUGA_PRO_VERSION', '1.3' );
+		define( 'TORTUGA_PRO_VERSION', '1.3.1' );
 
 		// Define Plugin Name.
 		define( 'TORTUGA_PRO_PRODUCT_ID', 56518 );
@@ -219,7 +219,7 @@ class Tortuga_Pro {
 
 		$options = Tortuga_Pro_Settings::instance();
 
-		if ( $options->get( 'license_key' ) <> '' ) :
+		if ( '' !== $options->get( 'license_key' ) ) :
 
 			$license_key = trim( $options->get( 'license_key' ) );
 
