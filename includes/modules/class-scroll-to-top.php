0890779f621e963bb1843a917d69725926ce0c57
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-09-16 10:54:01 +0200

    disable scroll to top button when AMP is active

--- includes/modules/class-scroll-to-top.php
+++ includes/modules/class-scroll-to-top.php
@@ -45,7 +45,7 @@ class Tortuga_Pro_Scroll_To_Top {
 		$theme_options = Tortuga_Pro_Customizer::get_theme_options();
 
 		// Call Credit Link function of theme if credit link is activated.
-		if ( true === $theme_options['scroll_to_top'] ) :
+		if ( true === $theme_options['scroll_to_top'] && ! self::is_amp() ) :
 
 			wp_enqueue_script( 'tortuga-pro-scroll-to-top', TORTUGA_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.js', array( 'jquery' ), TORTUGA_PRO_VERSION, true );
 
@@ -62,8 +62,8 @@ class Tortuga_Pro_Scroll_To_Top {
 		// Add Scroll to Top headline.
 		$wp_customize->add_control( new Tortuga_Customize_Header_Control(
 			$wp_customize, 'tortuga_theme_options[scroll_top_title]', array(
-				'label' => esc_html__( 'Scroll to Top', 'tortuga-pro' ),
-				'section' => 'tortuga_pro_section_footer',
+				'label'    => esc_html__( 'Scroll to Top', 'tortuga-pro' ),
+				'section'  => 'tortuga_pro_section_footer',
 				'settings' => array(),
 				'priority' => 10,
 			)
@@ -72,7 +72,7 @@ class Tortuga_Pro_Scroll_To_Top {
 		// Add Scroll to Top setting.
 		$wp_customize->add_setting( 'tortuga_theme_options[scroll_to_top]', array(
 			'default'           => false,
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'refresh',
 			'sanitize_callback' => 'tortuga_sanitize_checkbox',
 		) );
@@ -84,6 +84,13 @@ class Tortuga_Pro_Scroll_To_Top {
 			'type'     => 'checkbox',
 			'priority' => 20,
 		) );
+	}
+
+	/**
+	 * Checks if AMP page is rendered.
+	 */
+	static function is_amp() {
+		return function_exists( 'is_amp_endpoint' ) && is_amp_endpoint();
 	}
 }
 
