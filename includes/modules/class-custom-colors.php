e1b5615eebb5e13bbb4fa929eecf2939b6178032
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-03-01 11:52:52 +0100

    prepare color settings for CSS variables

--- includes/modules/class-custom-colors.php
+++ includes/modules/class-custom-colors.php
@@ -32,9 +32,6 @@ class Tortuga_Pro_Custom_Colors {
 		// Add Custom Color CSS code to custom stylesheet output.
 		add_filter( 'tortuga_pro_custom_css_stylesheet', array( __CLASS__, 'custom_colors_css' ) );
 
-		// Add Custom Color CSS code to the Gutenberg editor.
-		add_action( 'enqueue_block_editor_assets', array( __CLASS__, 'custom_editor_colors_css' ) );
-
 		// Add Custom Color Settings.
 		add_action( 'customize_register', array( __CLASS__, 'color_settings' ) );
 	}
@@ -53,8 +50,12 @@ class Tortuga_Pro_Custom_Colors {
 		// Get Default Fonts from settings.
 		$default_options = Tortuga_Pro_Customizer::get_default_options();
 
+		// Color Variables.
+		$color_variables = '';
+
 		// Set Link Color.
 		if ( $theme_options['link_color'] != $default_options['link_color'] ) {
+			$color_variables .= '--page-background-color: ' . $theme_options['page_bg_color'] . ';';
 
 			$custom_css .= '
 				/* Link and Button Color Setting */
@@ -457,71 +458,12 @@ class Tortuga_Pro_Custom_Colors {
 			} // End if().
 		} // End if().
 
+		// Set Color Variables.
+		if ( '' !== $color_variables ) {
+			$custom_css .= ':root {' . $color_variables . '}';
+		}
+
 		return $custom_css;
-	}
-
-	/**
-	 * Adds Color CSS styles in the Gutenberg Editor to override default colors
-	 *
-	 * @return void
-	 */
-	static function custom_editor_colors_css() {
-		$custom_css = '';
-
-		// Get Theme Options from Database.
-		$theme_options = Tortuga_Pro_Customizer::get_theme_options();
-
-		// Get Default Fonts from settings.
-		$default_options = Tortuga_Pro_Customizer::get_default_options();
-
-		// Set Primary Color.
-		if ( $theme_options['link_color'] !== $default_options['link_color'] ) {
-
-			$custom_css .= '
-				.has-primary-color,
-				.edit-post-visual-editor .editor-block-list__block a {
-					color: ' . $theme_options['link_color'] . ';
-				}
-				.has-primary-background-color {
-					background-color: ' . $theme_options['link_color'] . ';
-				}
-			';
-		}
-
-		// Set Title Color.
-		if ( $theme_options['title_color'] !== $default_options['title_color'] ) {
-
-			$custom_css .= '
-				.edit-post-visual-editor .editor-post-title__block .editor-post-title__input {
-					color: ' . $theme_options['title_color'] . ';
-				}
-			';
-		}
-
-		// Add Custom CSS.
-		if ( '' !== $custom_css ) {
-			wp_add_inline_style( 'tortuga-editor-styles', $custom_css );
-		}
-	}
-
-	/**
-	 * Change primary color in Gutenberg Editor.
-	 *
-	 * @return array $editor_settings
-	 */
-	static function change_primary_color( $color ) {
-		// Get Theme Options from Database.
-		$theme_options = Tortuga_Pro_Customizer::get_theme_options();
-
-		// Get Default Fonts from settings.
-		$default_options = Tortuga_Pro_Customizer::get_default_options();
-
-		// Set Primary Color.
-		if ( $theme_options['link_color'] !== $default_options['link_color'] ) {
-			$color = $theme_options['link_color'];
-		}
-
-		return $color;
 	}
 
 	/**
@@ -709,4 +651,3 @@ class Tortuga_Pro_Custom_Colors {
 
 // Run Class.
 add_action( 'init', array( 'Tortuga_Pro_Custom_Colors', 'setup' ) );
-add_filter( 'tortuga_primary_color', array( 'Tortuga_Pro_Custom_Colors', 'change_primary_color' ) );
--- tortuga-pro.php
+++ tortuga-pro.php
@@ -136,6 +136,9 @@ class Tortuga_Pro {
 		// Enqueue Tortuga Pro Stylesheet.
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_styles' ), 11 );
 
+		// Add Custom CSS code to the Gutenberg editor.
+		add_action( 'enqueue_block_editor_assets', array( __CLASS__, 'enqueue_editor_styles' ), 11 );
+
 		// Register additional Magazine Widgets.
 		add_action( 'widgets_init', array( __CLASS__, 'register_widgets' ) );
 
@@ -165,6 +168,33 @@ class Tortuga_Pro {
 			wp_enqueue_style( 'tortuga-pro', TORTUGA_PRO_PLUGIN_URL . 'assets/css/tortuga-pro.css', array(), TORTUGA_PRO_VERSION );
 		}
 
+		// Enqueue Custom CSS.
+		wp_add_inline_style( 'tortuga-pro', self::get_custom_css() );
+	}
+
+	/**
+	 * Enqueue Editor Styles
+	 *
+	 * @return void
+	 */
+	static function enqueue_editor_styles() {
+
+		// Return early if Tortuga Theme is not active.
+		if ( ! current_theme_supports( 'tortuga-pro' ) ) {
+			return;
+		}
+
+		// Enqueue Custom CSS.
+		wp_add_inline_style( 'tortuga-editor-styles', self::get_custom_css() );
+	}
+
+	/**
+	 * Return custom CSS for color and font variables.
+	 *
+	 * @return void
+	 */
+	static function get_custom_css() {
+
 		// Get Custom CSS.
 		$custom_css = apply_filters( 'tortuga_pro_custom_css_stylesheet', '' );
 
@@ -174,8 +204,7 @@ class Tortuga_Pro {
 		$custom_css = preg_replace( '/\n/', '', $custom_css );
 		$custom_css = preg_replace( '/\t/', '', $custom_css );
 
-		// Add Custom CSS.
-		wp_add_inline_style( 'tortuga-pro', $custom_css );
+		return $custom_css;
 	}
 
 	/**
