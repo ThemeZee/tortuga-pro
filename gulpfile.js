3fc5bc4f4e7d4ca766b998348333685e568e4d3e
Thomas Weichselbaumer <t.weichselbaumer@inpsyde.com>
2025-01-02 15:29:58 +0100

    Update JS tooling and Sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var rename       = require( 'gulp-rename' );
 var replace      = require( 'gulp-replace' );
 var uglify       = require( 'gulp-uglify' );
 var rtlcss       = require( 'gulp-rtlcss' );
-var sass         = require( 'gulp-sass' );
+var sass         = require('gulp-sass')(require('sass'));
 var postcss      = require( 'gulp-postcss' );
 var sorting      = require( 'postcss-sorting' );
 
